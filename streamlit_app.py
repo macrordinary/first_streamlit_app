e1987ffaebeec556d39ffdad8e71dfe06cce4b1f
Brent Allen <37672880+macrordinary@users.noreply.github.com>
2023-08-13 12:52:58 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,9 +41,9 @@ except URLError as e:
 # streamlit.text(fruityvice_response.json())
 
 def get_fruit_load_list():
-  with my_cnx.cursor() as my_cur
-  my_cur.execute("select * from fruit_load_list")
-  return my_cur.fetchall()
+  with my_cnx.cursor() as my_cur:
+    my_cur.execute("select * from fruit_load_list")
+    return my_cur.fetchall()
 
 
 my_data_rows = get_fruit_load_list()
@@ -53,7 +53,7 @@ streamlit.dataframe(my_data_rows)
 # streamlit.stop()
 
 def insert_row_snowflake(new_fruit):
-  with my_cnx.cursor() as my_cur
+  with my_cnx.cursor() as my_cur:
     my_cur.execute("insert into fruit_load_list_values (add_my_fruit)")    
     return 'Thanks for adding ', new_fruit
 
